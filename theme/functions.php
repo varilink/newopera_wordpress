2605814c130e87bb7be4549612706309d64941a5
David Williamson <david.williamson@varilink.co.uk>
2023-05-10 11:14:39 +0100

    CREATE TWENTYTWENTYTHREE CHILD THEME FOR SITE

---
+++ theme/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! function_exists('varilink_enqueue_styles') ) {
+    function varilink_enqueue_styles() {
+        wp_enqueue_style(
+            'theme_style', get_template_directory_uri(), '/style.css'
+        );
+    }
+    add_action('wp_enqueue_scripts', 'varilink_enqueue_styles');
+}
+
+?>

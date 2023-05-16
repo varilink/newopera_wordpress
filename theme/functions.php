c61289add7bf46e3ab32f647adf0eff2346d1dcb
David Williamson <david.williamson@varilink.co.uk>
2023-05-16 19:12:42 +0100

    CORRECT BUG PREVENTING ENQUEUE OF CHILD THEME STYLESHEET
    The child theme stylesheet was not being successfully enqueued. It turns
    out that get_template_directory_uri() returns the directory for the
    parent theme and we needed to use get_stylesheet_directory_uri() to get
    the directory for the child theme instead.

--- theme/functions.php
+++ theme/functions.php
@@ -3,7 +3,7 @@
 if ( ! function_exists('varilink_enqueue_styles') ) {
     function varilink_enqueue_styles() {
         wp_enqueue_style(
-            'theme_style', get_template_directory_uri(), '/style.css'
+            'theme_style', get_stylesheet_directory_uri() . '/style.css'
         );
     }
     add_action('wp_enqueue_scripts', 'varilink_enqueue_styles');

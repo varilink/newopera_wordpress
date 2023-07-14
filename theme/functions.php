98a7ae547f929bebfac71282cc3bcd635de80c35
David Williamson <david.williamson@varilink.co.uk>
2023-07-14 03:37:59 +0100

    CHANGES TO THE THEME FUNCTIONS WP_ENQUEUE_SCRIPTS ACTION
    Four changes as follows:
    
      - Drop irrelevant check that the function doesn't already exist.
    
      - Simplify the structure by not having a separate named function.
    
      - Use a more distinct hook name to decrease the chance of a name
        clash.
    
      - Add an inline style when there are no upcoming productions that will
        hide any content that has the CSS class upcoming-productions.

--- theme/functions.php
+++ theme/functions.php
@@ -1,12 +1,20 @@
 <?php
 
-if ( ! function_exists('varilink_enqueue_styles') ) {
-    function varilink_enqueue_styles() {
-        wp_enqueue_style(
-            'theme_style', get_stylesheet_directory_uri() . '/style.css'
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'newopera-site-style', get_stylesheet_directory_uri() . '/style.css'
+    );
+    if ( is_front_page() ) {
+        $my_query = new WP_Query(
+            array('post_type' => 'upcoming-production')
         );
+        if ( ! $my_query -> have_posts() ) {
+            wp_add_inline_style(
+                'newopera-site-style',
+                '.upcoming-productions{display:none}'
+            );
+        }
     }
-    add_action('wp_enqueue_scripts', 'varilink_enqueue_styles');
-}
+});
 
 ?>

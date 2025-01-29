3b2357c32bb36ef36dfe69be9a92c09d103ae9eb
David Williamson <david.williamson@varilink.co.uk>
2025-01-29 11:44:05 +0000

    INITIAL WORK ON THE NEW, STAND-ALONE THEME
    The theme was a child theme, but now we've made it stand-alone. This is
    a work in progress but I am committing the work to date as I want to
    roll it out to the customer testing host for them to see.

--- theme/functions.php
+++ theme/functions.php
@@ -17,4 +17,41 @@ add_action('wp_enqueue_scripts', function () {
     }
 });
 
+function convert_to_json_safe_string( $input ) {
+
+    // Encode the string to JSON, which escapes newlines, quotes, etc.
+    $json_safe = json_encode( $input );
+    return $json_safe;
+
+}
+
+
+add_action('init', function () {
+
+    register_block_pattern_category(
+        'newopera',
+        array( 'label' => 'New Opera Company, Derby', 'slug' => 'newopera' )
+    );
+
+    $banner = <<<END
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+
+    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+    <div class="wp-block-group alignwide" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+
+        <!-- wp:site-logo {"width":480,"shouldSyncIcon":false} /-->
+        <!-- wp:navigation {"ref":25} /-->
+
+    </div>
+    <!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
+END;
+
+    
+
+} );
+
 ?>
